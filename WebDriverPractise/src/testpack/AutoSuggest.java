a84f68307b583770274f1f75a692d1da9c274c42
Priya <Priya@LAPTOP-U1UV58Q9>
2020-02-22 09:45:55 +0530

    initial

--- WebDriverPractise/src/testpack/AutoSuggest.java
+++ WebDriverPractise/src/testpack/AutoSuggest.java
@@ -10,7 +10,7 @@ public class AutoSuggest {
 
 	public static void main(String[] args) {
 	
-	int j = 1;
+	int j = 1; 
 
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Priya\\Desktop\\driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
